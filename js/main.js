7da50273624192e1c05c8327db3c03a005e999e5
Ruslan Yakushev <yakushev.rusl101@gmail.com>
2020-01-22 04:46:35 +0200

    main page

--- js/main.js
+++ js/main.js
@@ -29,15 +29,21 @@ var main_inner = $(".main__inner");
 main_inner.css("left",  "-" + main_inner.width() + "px");
 main_inner.css("top", "-" + main_inner.height() + "px");
 
-$(".main__inner").animate( {"left": "-" + main_inner.width() / 7, "top" : "-" + main_inner.height() / 4.9}, 1400);
-
+$(".main__inner").animate( {"left": "-" + main_inner.width() / 7, "top" : "-" + main_inner.height() / 4.9}, 1000);
 $(".main__item").toggleClass('main__item_scale');
+
+
+setTimeout(function() {
+	$(".main__item_r").css("transform", "translateX(1px) scale(1)");
+	$(".main__item_l").css("transform", "translateX(-1px) scale(1)");
+
+}, 100);
 
 function animate_open(element, url, open_type) {
 	$(element).addClass("animation_1");
 	$(".main").addClass("animation_whitefade");
 	$(element).find(".main__cont").addClass("animation_cont");
-
+	
 	setTimeout(function(){
 		window.open(url, open_type);
 	}, 1450);
