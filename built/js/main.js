459dcb226dd66cf85413b1afd550bcb6a1b0fa08
Ruslan Yakushev <yakushev.rusl101@gmail.com>
2020-02-15 05:58:55 +0200

    migrate to new gulp-projects-starter

---
+++ built/js/main.js
@@ -0,0 +1,123 @@
+function animate_link_self(query, url) {
+    document.querySelector(query).classList.add("animation_whitefade");
+    setTimeout(function () {
+        window.open(url, "_self");
+    }, 400);
+}
+function animate_link_blank(query, url) {
+    var el = document.querySelector(query);
+    el.classList.add("animation_whitefade");
+    setTimeout(function () {
+        window.open(url, "_blank");
+        el.classList.remove("animation_whitefade");
+    }, 150);
+    document.querySelector(".main__messageme").style.color = "#202020";
+}
+function animate_link(query, url, open_type) {
+    document.querySelector(".main__messageme").style.color = "white";
+    if (open_type != "_blank") {
+        animate_link_self(query, url);
+    }
+    else {
+        animate_link_blank(query, url);
+    }
+}
+function menu_func(menu) {
+    menu.style.top = "-1px";
+}
+function main_func() {
+    var main_inner = document.querySelector(".main__inner");
+    var items = document.querySelectorAll(".main__item");
+    main_inner.style.left = "-" + main_inner.clientWidth / 7;
+    main_inner.style.top = "-" + main_inner.clientHeight / 4.9;
+    for (var i = 0; i < items.length; i++) {
+        items[i].classList.add("main__item_scale");
+    }
+    setTimeout(function () {
+        document.querySelector(".main__item_r").style.transform = "translateX(1px) scale(1)";
+        document.querySelector(".main__item_l").style.transform = "translateX(-1px) scale(1)";
+    }, 100);
+    set_animate_link();
+    set_animate_panel();
+}
+function set_animate_panel() {
+    var animate_pannels = [".main__item"];
+    for (var i = 0; i < animate_pannels.length; i++) {
+        var all_el = document.querySelectorAll(animate_pannels[i]);
+        for (var j = 0; j < all_el.length; j++) {
+            if (all_el[j].classList.contains("main__item_profile"))
+                continue;
+            all_el[j].onclick = function () {
+                var href = this.getAttribute("href");
+                event.target.classList.add("animation_item");
+                animate_link(".main", href, "_self");
+            };
+        }
+    }
+}
+function set_animate_link() {
+    var animate_buttons = [".main__messageme", ".social__link"];
+    for (var i = 0; i < animate_buttons.length; i++) {
+        var all_el = document.querySelectorAll(animate_buttons[i]);
+        for (var j = 0; j < all_el.length; j++) {
+            all_el[j].onclick = function () {
+                var href = this.getAttribute("href");
+                var target = this.getAttribute("target");
+                animate_link(".main", href, target);
+            };
+        }
+    }
+}
+function check_if_email(str) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(str))
+        return (true);
+    return (false);
+}
+function submit() {
+    var url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdfogzpcJWXMB_wCdbBvIsSHNeroCzOmGkX1PoY3gDaGFGwVw/formResponse";
+    var xhttp = new XMLHttpRequest();
+    var mail = document.querySelector("#email").value;
+    var name = document.querySelector("#name").value;
+    var message = document.querySelector("#message").value;
+    if (name === "") {
+        show_error(document.querySelectorAll(".contactme__inpwrapper")[0], "This field is required");
+        return -1;
+    }
+    if (mail === "") {
+        show_error(document.querySelectorAll(".contactme__inpwrapper")[1], "This field is required");
+        return -1;
+    }
+    if (!check_if_email(mail)) {
+        show_error(document.querySelectorAll(".contactme__inpwrapper")[1], "Should be an actual email address");
+        return -1;
+    }
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("emailAddress=" + mail + "&entry.2109138769=" + name + " &entry.1400602646= " + message);
+    animate_form();
+}
+function hide_all_errors_contactme() {
+    var errors = document.querySelectorAll(".contactme__inpwrapper");
+    for (var i = 0; i < errors.length; i++) {
+        errors[i].querySelector(".contactme__errorwrapper").classList.remove("contactme__showerror");
+    }
+}
+function show_error(el, error) {
+    hide_all_errors_contactme();
+    var subel = el.querySelector(".contactme__errorwrapper");
+    subel.classList.add("contactme__showerror");
+    subel.querySelector(".contactme__text").innerText = error;
+}
+function animate_form() {
+    document.querySelector(".thankyou-note").classList.add("fadein_anim");
+    document.querySelector(".contactme").classList.add("fadeout_anim");
+}
+(function () {
+    var menu = document.querySelector(".menu");
+    if (document.querySelector(".main") !== null)
+        main_func();
+    else if (menu !== null)
+        menu_func(menu);
+})();
+
+//# sourceMappingURL=../maps/main.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,148 +1,315 @@
-/* system */
 const path = require('path');
+
+/*================================== SETTINGS ===============================*/
 const proj_name = path.resolve(__dirname, '.').split(path.sep).pop();
+const built_folder = "built";
+
+const proj_path = "./" + proj_name;
+const built_path = proj_path + "/"+ built_folder;
+
+const js_version = "es5";
+
+const files_to_ssh = built_path + "/**/*";
+const sshDir = '/var/www/html/';
+const sshConfig = require('./../gulpfile.js').sshConfig;
+/*
+const sshConfig = {
+  host: '',
+  port: 22,
+  username: '',
+  password: ''
+};
+*/
+
+const min_dir = proj_path + "/min";
+const purify_css_html = built_path +"/**/*.html";
+
+/*------------------------------------- HTML -------------------------------------*/
+const pages_files = proj_path + "/pages/**/*.html";
+const pages_dest_dir = built_path;
+const min_pages_dest_dir = min_dir;
+
+/*------------------------------------- CSS -------------------------------------*/
+const scss_files = proj_path + "/scss/*.{scss,css}";
+const css_dest_dir = built_path +"/css";
+const min_css_dest_dir = min_dir + "/css";
+
+/*------------------------------------- JS -------------------------------------*/
+const ts_files = proj_path + "/ts/*.{ts,js}";
+const js_dest_dir = built_path +"/js";
+const min_js_dest_dir = min_dir + "/js";
+
+/*------------------------------------- IMG -------------------------------------*/
+const img_files = proj_path + "/img/**/*";
+const img_dest = built_path +"/img";
+const min_img_dest = min_dir +"/img";
+
+/*------------------------------------- FONTS -------------------------------------*/
+const fonts_files = proj_path + "/fonts/**/*";
+const fonts_dest = built_path +"/fonts";
+const min_fonts_dest = min_dir +"/fonts";
+
+/*======================================== PRESET ===============================*/
+/* system */
+const fs = require('fs');
 
 /* global */
 const { src, dest, parallel, task } = require('gulp');
 const gwatch = require('gulp').watch;
 const browSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
-const ssh = require('gulp-ssh')
 
 /* css */
 const sass = require('gulp-sass');
 const cssbeautify = require('gulp-cssbeautify');
 const autoprefixer = require('gulp-autoprefixer');
 const sassGlob = require('gulp-sass-glob');
+const cssPurify = require('gulp-purgecss');
+
 
 /* js */
+const ts = require('gulp-typescript');
 const jsinclude = require('gulp-include')
 
-/* ----------------------------------files to compile---------------------------------------------*/
-//let html_files = ["index.html", "pages/resume.html", "pages/contacts.html", "pages/portfolio.html", "pages/about.html"];
-let html_files = ["index.html", "pages/resume.html", "pages/contacts.html", "pages/rsm.html", "pages/portfolio.html", "pages/contactme.html" ];
-let css_files = ["main.scss", "resume.scss", "contacts.scss", "main-bg.scss", "index.scss", "menu.scss", "portfolio.scss", "contactme.scss"];
-/* -----------------------------------------------------------------------------------------------*/
 
-const sshConfig = require('./../gulpfile.js').sshConfig;
-var gulpSSH = new ssh({
-  ignoreErrors: false,
-  sshConfig: sshConfig
-});
+/* html */
+const fileinclude = require('gulp-file-include');
 
-function css(m, fl){
-  
-  return src("./" + proj_name + "/scss/*.scss")
+/*======================================== FUNCTIONS ===============================*/
+function css() {
+  let purify_files = [
+        purify_css_html
+  ];
+
+  return src(scss_files)
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(cssbeautify())
+    /*
+    .pipe(cssPurify(
+      { 
+        content: purify_files
+      }
+    ))
+    */
     .pipe(sourcemaps.write('../maps'))
-    .pipe(dest("./" + proj_name + "/build/css"))
-    .pipe(browSync.stream())
-    .pipe(gulpSSH.dest('/var/www/html/build/css'))
+    .pipe(dest(css_dest_dir))
+    .pipe(browSync.stream())        
 }
 
-function sscss(m=false) {
-  console.log("Yes");
-  for (let i = 0; i < css_files.length; i++){
-    compile_css(m, css_files[i]);
-  }
-}
-
-function js(m=false) {
-
-  return src("./" + proj_name + "/js/main.js", { sourcemaps: true })
+function js() {
+  return src(ts_files, { sourcemaps: true })
     .pipe(sourcemaps.init())
     .pipe(jsinclude()).on('error', console.log)
+    .pipe(ts(
+      {
+        noImplicitAny: true,
+        target: js_version, 
+        removeComments: false, 
+        allowJs: true
+      }
+    ))
     .pipe(sourcemaps.write('../maps'))
-    .pipe(dest('./' + proj_name + '/build/js', { sourcemaps: true }))
+    .pipe(dest(js_dest_dir), { sourcemaps: true })
+    .pipe(browSync.stream());
+}
+
+function pages(){
+  return src([pages_files])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest(pages_dest_dir))
     .pipe(browSync.stream())
-    .pipe(gulpSSH.dest('/var/www/html/build/js'))
-    
 }
 
-function compile_html(path){
-  let to = path.lastIndexOf('/');
-  let folder;
-
-  if(to > 0) {
-    folder = path.substring(0, to);
-  } else {
-    folder = "";
-  }
-
-  return src("./" + proj_name + "/" + path)
-   .pipe(gulpSSH.dest('/var/www/html/' + folder))
-}
-
-function html() {
-  for(let i = 0; i < html_files.length; i++) {
-    compile_html(html_files[i]);  
-  }
-}
 
 function img(){
-  return src("./" + proj_name + "/img/**/*")
-    .pipe(gulpSSH.dest('/var/www/html/img/'));
+  return src(img_files)
+    .pipe(dest(img_dest))
+    .pipe(browSync.stream())
 }
 
-function resume(){
-  return src("./" + proj_name + "/data/**/*")
-    .pipe(gulpSSH.dest('/var/www/html/data/'));
+function fonts(){
+  return src(fonts_files)
+    .pipe(dest(fonts_dest))
+    .pipe(browSync.stream());
 }
 
-function watch() {
+function minify_css(){
+  const cleanCSS = require('gulp-clean-css');
+  
+  let purify_files = [
+        purify_css_html
+  ];
+
+  return src(scss_files)
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssPurify(
+      { 
+        content: purify_files
+      }
+    ))
+    .pipe(
+      cleanCSS({compatibility: 'ie10', debug: true}, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      })
+    )
+    .pipe(dest(min_css_dest_dir))
+}
+
+function minify_js() {
+  const uglify = require('gulp-uglify');
+  const pipeline = require('readable-stream').pipeline;
+
+  return pipeline(
+    src(ts_files), 
+    jsinclude().on('error', console.log),
+    ts(
+      {
+        noImplicitAny: true,
+        target: js_version, 
+        removeComments: false, 
+        allowJs: true
+      }
+    ),
+    uglify(),
+    dest(min_js_dest_dir)
+  );
+}
+
+function minify_fonts() {
+  return src(fonts_files)
+    .pipe(dest(min_fonts_dest))
+}
+
+function minify_img() {
+  const imagemin = require('gulp-imagemin');
+  
+  src(img_files)
+    .pipe(imagemin())
+    .pipe(dest(min_img_dest));
+}
+
+function minify_pages() {
+  const htmlmin = require('gulp-htmlmin');
+
+  return src([pages_files])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(htmlmin(
+      { 
+        collapseWhitespace: true, 
+        removeComments: true, 
+        caseSensitive: true,
+        minifyCSS: true,
+        minifyJS: true, 
+        minifyURLs: true, 
+        removeEmptyAttributes: true,
+        removeScriptTypeAttribute: true, 
+        removeStyleLinkTypeAttributes: true,
+        //sortClassName: true, 
+        useShortDoctype: true
+
+      }
+    ))
+    .pipe(dest(min_pages_dest_dir))
+}
+
+function minify_all() {
+  minify_fonts();
+  minify_img();
+  minify_pages();
+  minify_js();
+  minify_css();
+}
+
+function compile_all() {
+  pages();
+  css();
+  js();
+  fonts();
+  img();
+}
+
+function initBrowser() {
   browSync.init({
     port: 3000, 
     server: {
-      baseDir: './' + proj_name
-    } 
+      baseDir: built_path + "/",
+    },
   });
-
-  gwatch("./" + proj_name + "/data/**/*", resume);
-  gwatch('./' + proj_name + '/scss/**/*', css);
-  gwatch('./' + proj_name + '/img/**/*', img);
-  
-  gwatch('./' + proj_name + '/js/**/*.js', js);
-  gwatch('./' + proj_name + '/*.html').on('change', browSync.reload).on('change', html);
-  gwatch('./' + proj_name + '/pages/**/*.html').on('change', browSync.reload).on('change', html);
+}
+function watch() {
+  gwatch(fonts_files, fonts);
+  gwatch(img_files, img);
+  gwatch(pages_files, pages);
+  gwatch(scss_files, css);
+  gwatch(ts_files, js);
 }
 
-task(proj_name + "_mj", function() { 
+function clean_all(){
+  return src(built_path +"/*")
+    .pipe(require('gulp-clean')());
+}
+
+function ssh_all() {
+  const ssh = require('gulp-ssh');
+  const gulpSSH = new ssh ({
+    ignoreErrors: false,
+    sshConfig: sshConfig
+  });
+
+  return src(files_to_ssh)
+    .pipe(gulpSSH.dest(sshDir));
+}
+
+/*======================================== TASKS ===============================*/
+task(proj_name, function() { 
   return new Promise(function(resolve, reject) {
-    css();
-    js(true);
-    console.log("js minified");
+    initBrowser()
+    compile_all();
+    watch();
     resolve();
   });
 });
 
-task(proj_name + "_mc", function() { 
+task(proj_name + "_c", function() { 
   return new Promise(function(resolve, reject) {
-    css(true);
-    js();
-    console.log("css minified");
-    resolve();
+    console.log("Cleaning...");
+    clean_all();
   });
 });
 
 task(proj_name + "_m", function() { 
   return new Promise(function(resolve, reject) {
-    css();
-    js();
-    console.log("js and css minif ied");
-    resolve();
+    console.log("Minifying...");
+    minify_all();
   });
 });
 
-task(proj_name, function() { 
+task(proj_name + "_s", function() { 
   return new Promise(function(resolve, reject) {
-    resume();
-    css();
-    js();
-    html();
-    watch();
-    resolve();
+    console.log("Transferring over ssh...");
+    
+
+    ssh_all();
+  });
+});
+
+task(proj_name + "_hlp", function() { 
+  return new Promise(function(resolve, reject) {
+    console.log("--hlp            Display this message");
+    console.log("--m              Minify css and js");
+    console.log("--c              Clean built directory");
+    console.log("--s              Transfer built directory to the server using ssh");
   });
 });
